ae6614320553c50de02d18f0714a6da896a19a6b
Anubhav575 <anubhavsinghcontact@gmail.com>
2026-02-11 10:08:19 +0530

    Update c by mayank3.c

--- c by mayank3.c
+++ c by mayank3.c
@@ -8,7 +8,8 @@
       C=(2*3.14*R);
    printf(" circumference of circle %f",C);
       A=(3.14*R*R);
-   printf("area of circle is %f",A); asd
+   printf("area of circle is %f",A); 
    return 0;
 
    }
+
